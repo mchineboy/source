f91a5810383c8d1f94b01d069f20c97d9b8d98b8
Tyler Hardison <tyler@redhawksecurity.com>
2017-12-21 12:21:13 -0800

    go go

--- basicBot.js
+++ basicBot.js
@@ -56,7 +56,7 @@
 
     var loadChat = function(cb) {
         if (!cb) cb = function() {};
-        $.get('https://rawgit.com/mchineboy/source/master/lang/langIndex.json', function(json) {
+        $.get('https://cdn.rawgit.com/mchineboy/source/0408344/lang/langIndex.json', function(json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== 'undefined') {
                 langIndex = json;
@@ -194,9 +194,9 @@
         status: false,
         name: 'basicBot',
         loggedInID: null,
-        scriptLink: 'https://rawgit.com/mchineboy/source/master/basicBot.js',
+        scriptLink: 'https://cdn.rawgit.com/mchineboy/source/0408344/basicBot.js',
         cmdLink: 'http://git.io/245Ppg',
-        chatLink: 'https://rawgit.com/mchineboy/source/master/lang/en.json',
+        chatLink: 'https://cdn.rawgit.com/mchineboy/source/0408344/lang/en.json',
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
@@ -204,8 +204,8 @@
         settings: {
             botName: 'basicBot',
             language: 'english',
-            chatLink: 'https://rawgit.com/mchineboy/source/master/lang/en.json',
-            scriptLink: 'https://rawgit.com/mchineboy/source/master/basicBot.js',
+            chatLink: 'https://cdn.rawgit.com/mchineboy/source/0408344/lang/en.json',
+            scriptLink: 'https://cdnrawgit.com/mchineboy/source/0408344/basicBot.js',
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
@@ -265,9 +265,9 @@
             tokeInProgress: false,
             tokers: [],
             blacklists: {
-                NSFW: 'https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json',
-                OP: 'https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json',
-                BANNED: 'https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json'
+                NSFW: 'https://cdn.rawgit.com/mchineboy/source/0408344/blacklists/NSFWlist.json',
+                OP: 'https://cdn.rawgit.com/mchineboy/source/0408344/blacklists/OPlist.json',
+                BANNED: 'https://cdn.rawgit.com/mchineboy/source/0408344/blacklists/BANNEDlist.json'
             }
         },
         room: {
@@ -2721,7 +2721,7 @@
                         }));
                         var argument = msg.substring(cmd.length + 1);
 
-                        $.get('https://rawgit.com/basicBot/source/master/lang/langIndex.json', function(json) {
+                        $.get('https://cdn.rawgit.com/mchineboy/source/0408344/lang/langIndex.json', function(json) {
                             var langIndex = json;
                             var link = langIndex[argument.toLowerCase()];
                             if (typeof link === 'undefined') {
